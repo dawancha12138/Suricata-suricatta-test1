de06c02bc1c37d1d305efef357567e56a061b01a
dawancha12138 <18600242657@163.com>
2022-06-07 23:41:25 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,25 @@
-#include <Arduino.h>
+#include <WiFi.h>
+const char* ssid     = "Xiaomi_7C9C";//连接WIFI名(SSID)
+const char* password = "qwer1234";//WIFI密码
 
-void setup() {
-  // put your setup code here, to run once:
-  //123
-  //123
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("--------------");
+  
+  WiFi.begin(ssid,password);//启动网络连接
+
+  Serial.printf("Begin to Connect %s\r\n", ssid);
+  while (WiFi.status() != WL_CONNECTED)//检测网络是否连接成功
+  {
+    delay(500);
+    Serial.print(".");
   }
+  Serial.println();
 
-void loop() {
-  // put your main code here, to run repeatedly://123
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());//返回ESP8266的IP地址
 }
+
+void loop() {}
