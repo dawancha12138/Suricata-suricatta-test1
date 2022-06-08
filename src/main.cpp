a37ddf94d49a46c7f08ce6440634d31ce568c5b7
dawancha12138 <18600242657@163.com>
2022-06-08 11:14:00 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,29 @@
 #include <WiFi.h>
+#include "lwip/dns.h"
+#include <string.h>
+#include <sys/param.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "freertos/event_groups.h"
+#include "esp_system.h"
+#include "esp_wifi.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "nvs_flash.h"
+#include "esp_netif.h"
+#include "driver/gpio.h"
+#include "lwip/err.h"
+#include "lwip/sockets.h"
+#include "lwip/sys.h"
+#include "lwip/netdb.h"
+
 const char* ssid     = "Xiaomi_7C9C";//连接WIFI名(SSID)
 const char* password = "qwer1234";//WIFI密码
-
+    const struct addrinfo hints = {
+        .ai_family = AF_INET,           /* 指定返回地址的协议簇，AF_INET(IPv4)、AF_INET6(IPv6)、AF_UNSPEC(IPv4 and IPv6)*/
+        .ai_socktype = SOCK_STREAM,     /* 设定返回地址的socket类型，流式套接字 */     
+    }; 
+    char buf[100];  
 void setup()
 {
   Serial.begin(115200);
@@ -20,6 +42,41 @@ void setup()
 
   Serial.print("Connected, IP address: ");
   Serial.println(WiFi.localIP());//返回ESP8266的IP地址
+   struct addrinfo *result;
+    int err;
+    err = getaddrinfo("kidiota.f3322.net", "19132", &hints, &result);
+    if(err != 0)        /* 返回值不为0，函数执行失败*/
+        Serial.printf("getaddrinfo err: %d \n",err);
+                             /* 用来存储IP地址字符串 */
+    struct sockaddr_in  *ipv4 = NULL;   /* IPv4地址结构体指针 */
+        if(result->ai_family == AF_INET) 
+    {
+        ipv4 = (struct sockaddr_in *)result->ai_addr;//将得到的IP地址赋给新的IPv4结构体指针
+        inet_ntop(result->ai_family, &ipv4->sin_addr, buf, sizeof(buf));//将接受到的IP地址信息转换成为点分十进制IP地址存入buf字符串
+        Serial.printf("[IPv4]%s [port]%d \n",buf,ntohs(ipv4->sin_port));//打印转换后的IP地址和端口号
+    }
+    else
+        Serial.printf("got IPv4 err !!!\n");
 }
 
-void loop() {}
+void loop() {
+    Serial.print("Connecting to");
+  Serial.println(buf);
+  WiFiClient client;//tcp连接
+  const int httpPort=19132;//端口号
+  if(!client.connect(buf,httpPort)){//连接失败
+    Serial.println("connection failed");
+    Serial.println(WiFi.localIP());
+    return;
+  }
+  //向服务器发送请求
+  client.print("HelloServer!");
+  //读取返回值
+  while(client.available()){
+    String line=client.readStringUntil('\r');
+    Serial.print(line);
+  }
+  Serial.println();
+  Serial.println("closing connection");
+  delay(5000000);
+}
