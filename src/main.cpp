e85920ccdef67a23fb593833688db7cd3fd432b4
dawancha12138 <18600242657@163.com>
2022-06-09 08:39:37 +0800

    master

--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,6 @@
 #include <WiFi.h>
 #include "lwip/dns.h"
 #include <string.h>
-#include <sys/param.h>
-#include "freertos/FreeRTOS.h"
-#include "freertos/task.h"
-#include "freertos/event_groups.h"
-#include "esp_system.h"
-#include "esp_wifi.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "nvs_flash.h"
-#include "esp_netif.h"
-#include "driver/gpio.h"
-#include "lwip/err.h"
-#include "lwip/sockets.h"
-#include "lwip/sys.h"
-#include "lwip/netdb.h"
 #include <SS_gethostbyname.h>
 
 const char* ssid     = "Xiaomi_7C9C";//连接WIFI名(SSID)
